2a0effaee0d0cc97322781416abddc649a75de60
Gouthami <gouthamikaranam2108@gmail.com>
2026-07-13 17:27:11 -0700

    updating the Title name as Todo list

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,11 @@ import TodoList from './TodoList.jsx';
 import TodoForm from './TodoForm.jsx';
 
 function App() {
- 
-  
-
   return (
     <div>
-        <h1>My Todos</h1>
+        <h1>Todo List</h1>
         <TodoForm/>
-        <TodoList />     
+        <TodoList/>     
     </div>
   );
 }
