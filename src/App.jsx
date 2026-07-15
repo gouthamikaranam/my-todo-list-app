ff81052f68653873d379b3558b5aa961c739500c
Gouthami <gouthamikaranam2108@gmail.com>
2026-07-14 17:48:17 -0700

    indentation improved

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import './App.css';
-import TodoForm from './TodoForm.jsx';
-import TodoList from './TodoList.jsx';
+import "./App.css";
+import TodoForm from "./TodoForm.jsx";
+import TodoList from "./TodoList.jsx";
 
 function App() {
   return (
     <div>
-        <h1>Todo List</h1>
-        <TodoForm/>
-        <TodoList/>     
+      <h1>Todo List</h1>
+      <TodoForm />
+      <TodoList />
     </div>
   );
 }
 
-export default App
+export default App;
